e95bf73289342db5a9a9e367d6cab0dc6c16fd00
Jennifer <hr08032@ues.edu.sv>
2023-09-08 17:47:17 -0600

    fix recuperacion_clave.php title

--- Vista/recuperacion_clave.php
+++ Vista/recuperacion_clave.php
@@ -9,7 +9,7 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inicio de Sesión</title>
+    <title>Recuperación</title>
 
     <!-- Incluir Jquery -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>

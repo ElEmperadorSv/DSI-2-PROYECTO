e4693124d684bf369475975056e97f01bfc561dc
Huguix <ale97droid@gmail.com>
2023-09-24 00:46:27 -0600

    vista seleccion de rol

--- Controlador/login.php
+++ Controlador/login.php
@@ -1,37 +1,32 @@
 <?php
-    session_start();
-    header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+session_start();
+require_once('../Controlador/db_connection.php');
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 
-
-    if(isset($_SESSION['username'])) {
-        header("Location: ../Vista/home.php");
-        exit();
-    }
-
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        require_once('db_connection.php');
-
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        // Hash de la contraseña
-        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-
-        // Para verificar la contraseña
-        if (password_verify($password, $hashedPassword)) {
-            // La contraseña es válida
-        } else {
-            // La contraseña es incorrecta
+if (isset($_SESSION['username'])) {
+    header("Location: ../Vista/seleccion_rol.php");
+    exit();
 }
 
-        $query = "SELECT * FROM usuarios_dsi WHERE username = ? AND password = ?";
-        $stmt = $conn->prepare($query);
-        $stmt->bind_param("ss", $username, $password);
-        $stmt->execute();
-        $result = $stmt->get_result();
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    require_once('db_connection.php');
 
-        if ($result && mysqli_num_rows($result) > 0) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $query = "SELECT * FROM usuarios_dsi WHERE username = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result && $result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $hashedPassword = $row['password'];
+
+        if (password_verify($password, $hashedPassword)) {
             $_SESSION['username'] = $username;
-            header("Location: ../Vista/home.php");
+            header("Location: ../Vista/seleccion_rol.php");
             exit();
         } else {
             $message = "Usuario o contraseña incorrectos. Verifica que tus datos sean correctos.";
@@ -40,7 +35,12 @@
             exit();
         }
     } else {
-        header("Location: index.php");
+        $message = "Usuario o contraseña incorrectos. Verifica que tus datos sean correctos.";
+        $_SESSION['login_error'] = $message;
+        header("Location: ../index.php");
         exit();
     }
-?>
+} else {
+    header("Location: index.php");
+    exit();
+}
---
+++ Vista/seleccion_rol.php
@@ -0,0 +1,89 @@
+<?php
+session_start();
+require_once('../Controlador/db_connection.php');
+
+if (!isset($_SESSION['username'])) {
+    header("Location: ../index.php");
+    exit();
+}
+
+// Obtener los roles disponibles desde la base de datos
+$query = "SELECT * FROM roles_usuarios";
+$result = $conn->query($query);
+
+// Verificar si se obtuvieron resultados
+if ($result && $result->num_rows > 0) {
+    $roles_disponibles = array();
+    while ($row = $result->fetch_assoc()) {
+        $roles_disponibles[] = $row['nombre_rol'];
+    }
+} else {
+    $roles_disponibles = array(); // No se encontraron roles
+}
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Selección de Rol</title>
+
+    <!-- Incluir Jquery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="../Complementos/CSS/style.css">
+
+    <!-- Boxicons CSS -->
+    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" crossorigin="anonymous" />
+</head>
+
+<body class="bg-primary">
+    <main>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-5">
+                    <div class="card shadow-lg border-0 rounded-lg mt-5">
+                        <div class="card-header">
+                            <h3 class="text-center font-weight-light my-4">Selección de Rol</h3>
+                        </div>
+                        <div class="card-body">
+                            <form action="../Vista/home.php" method="POST">
+                                <div class="form-group">
+                                    <label for="rol">Selecciona tu rol:</label>
+                                    <select class="form-select" id="rol" name="rol">
+                                        <?php
+                                        // Mostrar los roles disponibles en el select
+                                        foreach ($roles_disponibles as $rol) {
+                                            echo "<option value='$rol'>$rol</option>";
+                                        }
+                                        ?>
+                                    </select>
+                                </div>
+                                <button type="submit" class="btn btn-primary">Aceptar</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <!-- JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+
+    <script src="../Complementos/JS/recuperacion_clave.js"></script>
+</body>
+
+</html>
